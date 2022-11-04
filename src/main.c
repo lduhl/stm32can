435f55ac3d67fd34c3b2393605b3c16687b2dd89
Andrei <duh@vnet.ee>
2022-11-04 21:32:58 +0200

    first commit

---
+++ src/main.c
@@ -0,0 +1,318 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "can.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define Button1 HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)
+#define Button2 HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1)
+#define Button3 HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2)
+#define Button4 HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3)
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+	CAN_TxHeaderTypeDef TxHeader;
+	CAN_RxHeaderTypeDef RxHeader;
+	uint8_t TxData[8] = {0,};
+	uint8_t RxData[8] = {0,};
+	uint32_t TxMailbox = 0;
+	uint8_t trans_str[30];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+	
+	
+/* 
+	������ ��� ����� ������ (��� ������ RX_FIFO_0)
+	��� ����� ������ ����� �� ��� �� �������� ��� �� ��������� ����� � ������� ������� HAL_CAN_GetRxMessage(...)
+	� ������ ��������.
+	�����!!!!!!  ��� ������ ����� � MX_CAN_Init() ��������� ��������� ������
+*/
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+    if(HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK)
+    {  
+			if(RxData[6] == 0x31){
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+			}
+			if(RxData[6] == 0x32){
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+			}
+				HAL_UART_Transmit(&huart1, (uint8_t*)trans_str, strlen((char*)RxData), 100);
+    }
+}
+void CAN_interrupt(void){}
+
+/* 
+	������ ��� ������
+*/
+void HAL_CAN_ErrorCallback(CAN_HandleTypeDef *hcan)
+{
+    uint32_t er = HAL_CAN_GetError(hcan);
+    sprintf(trans_str,"ER CAN %lu %08lX", er, er);
+    HAL_UART_Transmit(&huart1, (uint8_t*)trans_str, strlen(trans_str), 100);
+}
+
+void HAL_CAN_Send()
+{
+		while(HAL_CAN_GetTxMailboxesFreeLevel(&hcan) == 0);
+
+		if(HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+		{
+			HAL_UART_Transmit(&huart1, (uint8_t*)"ER SEND\n", 8, 100);
+		}
+		HAL_Delay(500);
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_CAN_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	/* 
+		��������� ��������� ���������� �� �������� ������
+		StdId � ��� ������������� ������������ �����.
+		ExtId � ��� ������������� ������������ �����. �� ����� ���������� ����������� ������� ���� ����� 0.
+		RTR = CAN_RTR_DATA � ��� ������� � ���, ��� �� ���������� ���� � ������� (Data Frame). ���� ������� CAN_RTR_REMOTE, ����� ��� ����� Remote Frame.
+		IDE = CAN_ID_STD � ��� ������� � ���, ��� �� ���������� ����������� ����. ���� ������� CAN_ID_EXT, ����� ��� ����� ����������� ����. � StdId ����� ����� ������� 0, � � ExtId �������� ����������� �������������.
+		DLC = 8 � ���������� �������� ���� ������������ � ����� (�� 1 �� 8).
+		TransmitGlobalTime � ��������� � Time Triggered Communication Mode, �� ��� �� ���������� ������� ����� 0.
+	*/
+	TxHeader.StdId = 0x07B0;
+	TxHeader.ExtId = 0;
+	TxHeader.RTR = CAN_RTR_DATA; //CAN_RTR_REMOTE
+	TxHeader.IDE = CAN_ID_STD;   // CAN_ID_EXT
+	TxHeader.DLC = 8;
+	TxHeader.TransmitGlobalTime = 0;
+	
+	RxHeader.StdId = 0x07B0;
+	RxHeader.ExtId = 0;
+	RxHeader.RTR = CAN_RTR_DATA; //CAN_RTR_REMOTE
+	RxHeader.IDE = CAN_ID_STD;   // CAN_ID_EXT
+	RxHeader.DLC = 7;
+	
+	
+//	for(uint8_t i = 0; i < 8; i++)
+//	{
+//			TxData[i] = (i + 10);
+//	}
+	/* ���������� ������� ������� ����� �������� ����������  */
+	HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING | CAN_IT_ERROR | CAN_IT_BUSOFF | CAN_IT_LAST_ERROR_CODE);
+	
+	HAL_CAN_Start(&hcan);
+	
+	TxHeader.StdId = 0x07B0;
+	TxHeader.DLC = 7;
+	TxData[0] = 0x44;
+	TxData[1] = 0x53;
+	TxData[2] = 0x46;
+	TxData[3] = 0x30;
+	TxData[4] = 0x30;
+	TxData[5] = 0x30;
+	TxData[6] = 0x33;
+	TxData[7] = 0x00;
+				
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		
+		if(Button1 == 0){
+			TxData[6] = 0x31;
+			HAL_CAN_Send();
+		}
+		if(Button2 == 0){
+			TxData[6] = 0x32;
+			HAL_CAN_Send();
+		}
+		if(Button3 == 0){
+			TxData[6] = 0x33;
+			HAL_CAN_Send();
+		}
+		if(Button4 == 0){
+			TxData[6] = 0x34;
+			HAL_CAN_Send();
+		}
+
+				
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL8;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM3 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM3) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
